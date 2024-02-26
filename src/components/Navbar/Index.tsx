5e9629368712f9bb9f2097e0a52e5a94fffa4606
Petrik Diak <diak@petrik.hu>
2024-02-26 12:47:32 +0100

    Navbar

--- src/components/Navbar/Index.tsx
+++ src/components/Navbar/Index.tsx
@@ -1,25 +1,29 @@
 import './Index.css'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
 
 export function Navbar(){
-    <nav class="navbar navbar-expand-lg bg-danger-subtle">
-        <div class="container-fluid">
-        <span class="navbar-brand">Fény Fauna Állatkert</span>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg bg-danger-subtle">
+        <div className="container-fluid">
+        <span className="navbar-brand">Fény Fauna Állatkert</span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-        <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="index.html">Főoldal</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+        <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="index.html">Főoldal</a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link" href="etlap.html">Étlap</a>
+        <li className="nav-item">
+        <a className="nav-link" href="etlap.html">Programok</a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link" href="nyitvatartas.html">Nyitvatartás</a>
+        <li className="nav-item">
+        <a className="nav-link" href="etlap.html">Állatok</a>
         </li>
-        <li class="nav-item">
-        <a class="nav-link" href="kapcsolat.html">Kapcsolat</a>
+        <li className="nav-item">
+        <a className="nav-link" href="nyitvatartas.html">Nyitvatartás</a>
+        </li>
+        <li className="nav-item">
+        <a className="nav-link" href="kapcsolat.html">Kapcsolat</a>
         </li>
         </ul>
         </div>
