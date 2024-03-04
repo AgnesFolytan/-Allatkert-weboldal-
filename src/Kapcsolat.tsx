3c4f3043e0d7e8cf45cbc55c1e4b9f165bc7a701
Petrik Diak <diak@petrik.hu>
2024-03-04 12:39:43 +0100

    kapcsolat adat

--- src/Kapcsolat.tsx
+++ src/Kapcsolat.tsx
@@ -2,9 +2,38 @@ import './Kapcsolat.css'
 
 function Kapcsolat(){
     return (
-        <div>
-          <h1>Kapcsolat</h1>
-          <div><p>Telefonszám: +364789128</p></div>
+      <div>
+        <table className="table">
+  <thead>
+    <tr><th>
+    <h1>Kapcsolat</h1>
+    </th></tr>
+  </thead>
+  <tbody>
+  <tr>
+    <td>
+      <p id='adat'>Nyíregyházi Állatpark Nonprofit Kft.<br></br><br></br>
+          4431 Sóstófürdő, Sóstói út<br></br><br></br>
+          GPS: Lat: 48.006351 Long: 21.724568<br></br><br></br><br></br><br></br>
+          Állatpark elérhetőségei:<br></br>
+          +36 20 297 6862 <br></br>+36 20 297 8938<br></br>
+          +36 42 479 702<br></br>+36 42 500 535</p>
+    </td>
+    <td>
+      <p>E-mail: <br></br><br></br>
+        Általános kérdések<br></br>
+        erdeklodes@sostozoo.hu<br></br><br></br>
+        Partnereinknek<br></br>
+        konyveles@sostozoo.hu<br></br><br></br>
+        Pénzügy és számlázás <br></br>
+        info@sostozoo.hu<br></br><br></br>
+        Szakmai kérdések<br></br>
+        office@sostozoo.hu
+    </p></td>
+  </tr>
+  </tbody>
+</table>
+         
         </div>    
       )
 }
