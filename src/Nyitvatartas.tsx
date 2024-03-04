13cbbf7d1127df3b2d1733a00107df65e3f4aa4e
Petrik Diak <diak@petrik.hu>
2024-03-04 13:07:04 +0100

    Nyitvatartas

--- src/Nyitvatartas.tsx
+++ src/Nyitvatartas.tsx
@@ -1,7 +1,42 @@
+import './Nyitvatartas.css'
+
 function Nyitvatartas(){
     return (
         <div>
-          <p>nyitvatartas</p>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>Állatkert</th>
+                <th>Belépés</th>
+                <th>Állatházak*</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Január</th>
+                <td>minden nap</td>
+                <td>9:00-16:00</td>
+                <td>15:00-ig</td>
+                <td>9:00-15:30</td>
+              </tr>
+              <tr>
+              <th scope="row">Ferbuár</th>
+                <td>minden nap</td>
+                <td>9:00-16:00</td>
+                <td>15:00-ig</td>
+                <td>9:00-15:30</td>
+              </tr>
+              <tr>
+                <th scope="row">Március</th>
+                <td>minden nap</td>
+                <td>9:00-16:00</td>
+                <td>15:00-ig</td>
+                <td>9:00-15:30</td>
+              </tr>
+            </tbody>
+          </table>
         </div>    
       )
 }
