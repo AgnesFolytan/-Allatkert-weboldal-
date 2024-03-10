7bdcde5271fc95968c1ba7b748b1e3dec73ecdd6
AgnesFolytan <folytanagnes@gmail.com>
2024-03-10 16:48:04 +0100

    Állatok első frissítés

--- src/Allatok.tsx
+++ src/Allatok.tsx
@@ -1,9 +1,57 @@
+import { useEffect, useState } from 'react'
+import { Animal, Animals } from './animals';
+
 function Allatok(){
-    return (
-        <div>
-          <p>Allatok</p>
-        </div>    
-      )
+
+  const [ animals, setAnimals ] = useState([] as Animals[])
+  const [ newAnimal, setNewAnimal ] = useState('')
+  const [ searchTerm, setSearchTerm ] = useState('')
+
+  useEffect(() => {
+    console.log('animals fetch effect')
+    async function load() {
+      const response = await fetch('/animals.json')
+      const animals = await response.json() as Animals;
+      setAnimals(animals.animals);
+    }
+    load()
+  }, [])
+
+  useEffect( ()=>{
+    console.log("Változás történt a animals tömbben..." + animals.length);
+    document.title = `Létszám: ${animals.length}`;
+  }, [animals] )
+
+  const kivalogatott = animals.filter( animal => animal.kategoria );
+
+  return <div>
+    <h3>Keresés:</h3>
+    <input type="text" placeholder="Add meg a keresendő Részleget!" 
+      onInput={ e => { 
+        setSearchTerm(e.currentTarget.value), 
+        console.log(e.currentTarget.value)
+        }
+      }
+    ></input>
+
+    <h3>Állataink:</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Állat</th>
+          <th>Részleg</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          kivalogatott.map(animal => <tr>
+          <td>{animal.fajta}</td>
+          <td>{animal.kategoria}</td>
+        </tr>
+        )}
+      </tbody>
+    </table>
+  </div>
 }
 
 export default Allatok;
