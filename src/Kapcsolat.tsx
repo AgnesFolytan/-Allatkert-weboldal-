f55b149250c00cc9fbfdc305c14a301b69c89993
Petrik Diak <diak@petrik.hu>
2024-03-11 11:58:12 +0100

    sink

--- src/Kapcsolat.tsx
+++ src/Kapcsolat.tsx
@@ -5,31 +5,38 @@ function Kapcsolat(){
       <div>
         <table className="table">
   <thead>
-    <tr><th>
+    <tr><th colSpan={2}>
     <h1>Kapcsolat</h1>
     </th></tr>
   </thead>
   <tbody>
   <tr>
     <td>
-      <p id='adat'>Nyíregyházi Állatpark Nonprofit Kft.<br></br><br></br>
+          Fény Fauna Állatpark Nonprofit Kft.<br></br><br></br>
           4431 Sóstófürdő, Sóstói út<br></br><br></br>
           GPS: Lat: 48.006351 Long: 21.724568<br></br><br></br><br></br><br></br>
           Állatpark elérhetőségei:<br></br>
-          +36 20 297 6862 <br></br>+36 20 297 8938<br></br>
-          +36 42 479 702<br></br>+36 42 500 535</p>
+          <a className='adat' href="tel:+36202976862">+36 20 297 6862</a> <br></br> <a className='adat' href="tel:+36202978938">+36 20 297 8938</a><br></br>
+          <a className='adat' href="tel:+3642479702">+36 42 479 702</a><br></br> <a className='adat' href="tel:+3642500535">+36 42 500 535</a>
     </td>
     <td>
-      <p>E-mail: <br></br><br></br>
+      E-mail: <br></br><br></br>
         Általános kérdések<br></br>
-        erdeklodes@sostozoo.hu<br></br><br></br>
+        <a className='adat' href="mailto:erdeklodes@fenyfauna.hu">erdeklodes@fenyfauna.hu</a><br></br><br></br>
         Partnereinknek<br></br>
-        konyveles@sostozoo.hu<br></br><br></br>
+        <a className='adat'href="mailto:konyveles@fenyfauna.hu" >konyveles@fenyfauna.hu</a><br></br><br></br>
         Pénzügy és számlázás <br></br>
-        info@sostozoo.hu<br></br><br></br>
+        <a className='adat'href="mailto:info@fenyfauna.hu">info@fenyfauna.hu</a><br></br><br></br>
         Szakmai kérdések<br></br>
-        office@sostozoo.hu
-    </p></td>
+        <a className='adat'href="mailto:office@fenyfauna.hu">office@fenyfauna.hu</a>
+    </td>
+    <td>Social media
+      <br></br>
+      <a href=''><img width={50} src='https://th.bing.com/th/id/R.26d9974a1feec9905a4e0d5e5ddf8db6?rik=Og1ujXM2C1AJHQ&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2fa%2fa5%2fInstagram_icon.png&ehk=1%2fZWXYn2nN%2fR80TOtcKH5SsdLkkUvMLrB%2fHUXRDHk9I%3d&risl=&pid=ImgRaw&r=0'></img></a>
+      <br></br>
+      <a href=''><img></img></a>
+      <a href=''><img width={50} src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_(2017).svg/2560px-YouTube_full-color_icon_(2017).svg.png'></img></a>
+      <a href=''></a></td>
   </tr>
   </tbody>
 </table>
